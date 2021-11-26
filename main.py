d9d7bdbd6b3ce7cf6fd8d12b86a0acddb19bafb4
Usmanovlinar <chevianchik@gmail.com>
2021-11-27 01:05:59 +0300

    without commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print('hello hirls')
-print('hello my name is Linar')
-print('hellp')
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_1", type= str)
+parser.add_argument("way", type= str)
+args = parser.parse_args()
+folder =[]
+for i in os.walk(args.way):
+    folder.append(i)
+for address, dirs, files in folder:
+    for file in files:
+        if args.file_1 == file:
+            print(address+'/' + file)
+            break
