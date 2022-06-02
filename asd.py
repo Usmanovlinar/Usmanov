ac8f561301108d321b9d14ffbf435f33732932d8
Usmanovlinar <chevianchik@gmail.com>
2022-06-03 00:16:07 +0300

    homework

---
+++ asd.py
@@ -0,0 +1,77 @@
+stuffdict = {'banan':(300,75),
+             'piano':(500,80),
+             'bed':(400,100),
+             'phone':(200,50),
+             'lounch':(200,40),
+             'desk':(200,70),
+             'charge':(300,80),
+             'headphones':(200,30),
+             'clotches':(100,30),
+             'mouse':(200,60)
+            }
+def get_area_and_value(stuffdict):
+    area = [stuffdict[item][0] for item in stuffdict]
+    value = [stuffdict[item][1] for item in stuffdict]
+    return area, value
+
+
+def get_memtable(stuffdict, A=1000):
+    area, value = get_area_and_value(stuffdict)
+    n = len(value)  # находим размеры таблицы
+
+    # создаём таблицу из нулевых значений
+    V = [[0 for a in range(A + 1)] for i in range(n + 1)]
+
+    for i in range(n + 1):
+        for a in range(A + 1):
+            # базовый случай
+            if i == 0 or a == 0:
+                V[i][a] = 0
+
+            # если вес предмета меньше веса столбца,
+            # максимизируем значение суммарной ценности
+            elif area[i - 1] <= a:
+                V[i][a] = max(value[i - 1] + V[i - 1][a - area[i - 1]], V[i - 1][a])
+
+            # если вес предмета больше вес столбца,
+            # забираем значение ячейки из предыдущей строки
+            else:
+                V[i][a] = V[i - 1][a]
+    return V, area, value
+
+
+
+def get_selected_items_list(stuffdict, A=1000):
+    V, area, value = get_memtable(stuffdict)
+    n = len(value)
+    res = V[n][A]  # начинаем с последнего элемента таблицы
+    a = A  # начальный вес - максильмальный
+    items_list = []  # список сумм и ценностей
+
+    for i in range(n, 0, -1):  # идём в обратном порядке
+        if res <= 0:  # условие прерывания - собрали "рюкзак"
+            break
+        if res == V[i - 1][a]:  # ничего не делаем, двигаемся дальше
+            continue
+        else:
+            # "забираем" предмет
+            items_list.append((area[i - 1], value[i - 1]))
+            res -= value[i - 1]  # отнимаем значение ценности от общей
+            a -= area[i - 1]  # отнимаем сумму от общей
+
+    selected_stuff = []
+
+    # находим ключи исходного словаря - названия предметов
+    for search in items_list:
+        for key, value in stuffdict.items():
+            if value == search:
+                selected_stuff.append(key)
+
+    return selected_stuff
+
+stuff = get_selected_items_list(stuffdict)
+print(stuff)
+totarea = sum([stuffdict[item][0] for item in stuff])
+totvalue = sum([stuffdict[item][1] for item in stuff])
+print(totarea)
+print(totvalue)
